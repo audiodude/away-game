e4a28a66a8c2497544a838f1cd17d827c60ae5ec
Travis Briggs <audiodude@gmail.com>
2016-03-30 22:02:29 -0700

    Last changes for hackday

--- app.py
+++ app.py
@@ -9,10 +9,74 @@ TEAM_ABBR = [
   "sd", "sea", "sf", "stl", "tb", "tex", "tor", "was",
 ]
 
+TEAM_ABBR_AIRPORT_CODE = {
+  'atl':    'ATL',
+  'bal':    'BWI',
+  'bos':    'BOS',
+  'chc':    'ORD',
+  'cws':    'ORD',
+  'cin':    'CVG',
+  'cle':    'CLE',
+  'tex':    'DAL',
+  'col':    'DEN',
+  'det':    'DTW',
+  'hou':    'IAH',
+  'kc':     'MCI',
+  'laa':    'LAX',
+  'lad':    'LAX',
+  'mia':    'MIA',
+  'mil':    'MKE',
+  'min':    'MSP',
+  'nym':    'EWR',
+  'nyy':    'EWR',
+  'oak':    'OAK',
+  'ari':    'PHX',
+  'phi':    'PHL',
+  'pit':    'PIT',
+  'sd':     'SAN',
+  'sea':    'SEA',
+  'stl':    'STL',
+  'tb':     'TPA',
+  'tor':    'YYZ',
+  'sf':     'SFO',
+  'was':    'IAD',
+}
+
+AIRPORT_CODE_NAME = {
+  'MIA': 'Miami International Airport',
+  'ORD': "O'Hare International Airport",
+  'LAX': 'Los Angeles International Airport',
+  'BWI': 'Baltimore-Washington International Airport',
+  'MCI': 'Kansas City International Airport',
+  'OAK': 'Oakland International Airport',
+  'SFO': 'San Francisco International Airport',
+  'YYZ': 'Toronto Pearson International Airport',
+  'PIT': 'Pittsburgh International Airport',
+  'CVG': 'Cincinnati/Northern Kentucky International Airport',
+  'TPA': 'Tampa International Airport',
+  'STL': 'Lambert-St. Louis International Airport',
+  'BOS': 'Logan International Airport',
+  'ATL': 'Hartsfield-Jackson Atlanta International Airport',
+  'EWR': 'Newark Liberty International Airport',
+  'PHX': 'Phoenix Sky Harbor International Airport',
+  'PHL': 'Philadelphia International Airport',
+  'IAD': 'Washington Dulles International Airport',
+  'SAN': 'San Diego International Airport',
+  'DEN': 'Denver International Airport',
+  'DAL': 'Dallas Love Field',
+  'MKE': 'General Mitchell International Airport',
+  'IAH': 'George Bush Intercontinental Airport',
+  'MSP': 'Minneapolis-Saint Paul International Airport',
+  'SEA': 'Seattle-Tacoma International Airport',
+  'CLE': 'Cleveland Hopkins International Airport',
+  'DTW': 'Detroit Metropolitan Airport'
+}
+
 TEAMS = {
   'teams': [{
     'team': t,
-    'logo': 'logo_%s_79x76.jpg' % t,
+    'airport_code': TEAM_ABBR_AIRPORT_CODE[t],
+    'logo': 'logo_%s_79x76.jpg' % t
   } for t in TEAM_ABBR]
 }
          
@@ -30,9 +94,13 @@ def seatgeek():
 def teams():
   return flask.json.jsonify(TEAMS)
 
+@app.route('/api/local/airports')
+def airports():
+  return flask.json.jsonify(AIRPORT_CODE_NAME)
+
 @app.route('/')
 def index():
-  return flask.render_template('index.html')
+  return flask.send_from_directory('templates', 'index.html')
 
 if __name__ == "__main__":
   app.run(debug=True)
--- static/js/main.js
+++ static/js/main.js
@@ -1,1 +1,90 @@
-angular.module('AwayGameApp', ['ngMaterial']);
+agApp = angular.module('AwayGameApp', ['ngMaterial']);
+
+agApp.service('ApiService', function($http) {
+  this.getTeams = function() {
+    return $http.get('/api/local/teams');
+  };
+
+  this.getAirports = function() {
+    return $http.get('/api/local/airports');
+  };
+
+  this.qpx = function(date) {
+    return $http.get('/api/qpx/trips/search?date=' + date);
+  };
+});
+
+main = function($scope, ApiService) {
+  $scope.teams = [];
+  ApiService.getTeams().then(function(httpResp) {
+    $scope.teams = httpResp.data.teams;
+  });
+
+  $scope.airports = [];
+  ApiService.getAirports().then(function(httpResp) {
+    $scope.airports = httpResp.data;
+  });
+
+  $scope.selected = null;
+  $scope.selectTeam = function(abbr) {
+    $scope.selected = abbr;
+    angular.forEach($scope.teams, function(val) {
+      if (val.team == abbr) {
+        $scope.airport = val.airport_code
+      }
+    });
+    $scope.expandedIndex = 1;
+  };
+
+  $scope.isSelected = function(abbr) {
+    return abbr == $scope.selected;
+  };
+
+  $scope.expandedIndex = 0;
+  $scope.isExpanded = function(index) {
+    return $scope.expandedIndex == index;
+  };
+
+  $scope.submitAirport = function() {
+    $scope.expandedIndex = 2;
+  };
+
+  $scope.gameDate = null;
+
+  $scope.gameDetails = '--'
+  $scope.onDateChange = function() {
+    $scope.gameDetails = '@ Chicago White Sox, 7:15 PM CST';
+  };
+
+  $scope.flights = []
+  $scope.findFlights = function() {
+    // TODO: Turn the date selected into an YYYY-MM-DD, or whatever format
+    // QPX requires.
+
+    var data = null;
+    ApiService.qpx('2016-05-02').then(function(httpResp) {
+      data_day_before = httpResp.data;
+    });
+    ApiService.qpx('2016-05-03').then(function(httpResp) {
+      data = httpResp.data;
+      angular.forEach(data.trips.tripOption, function(option) {
+        var flight = {};
+        if (option.slice[0].segment.length > 1) {
+          return true;
+        }
+        flight.price = option.saleTotal.replace('USD', '$');
+        leg = option.slice[0].segment[0].leg[0]
+        flight.carrier = option.slice[0].segment[0].carrier
+        flight.origin = leg.origin;
+        flight.destination = leg.destination;
+        flight.arrivalTime = leg.arrivalTime;
+        flight.departureTime = leg.departureTime;
+        $scope.flights.push(flight)
+      });
+    });
+  };
+
+  $scope.eventSources = [];
+};
+
+agApp.controller('MainController', ['$scope', 'ApiService', main]);
